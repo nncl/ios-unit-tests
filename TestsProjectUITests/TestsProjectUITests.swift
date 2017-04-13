1a38398c8a6338192ed835b64d3705e4558562a2
Usuário Convidado <Guest@pa06micro16.local>
2017-04-12 22:55:32 -0300

    Adding UI test

--- TestsProjectUITests/TestsProjectUITests.swift
+++ TestsProjectUITests/TestsProjectUITests.swift
@@ -9,6 +9,8 @@
 import XCTest
 
 class TestsProjectUITests: XCTestCase {
+    
+    var app = XCUIApplication!
         
     override func setUp() {
         super.setUp()
@@ -18,8 +20,20 @@ class TestsProjectUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
+        app = XCUIApplication() // Acesso à app fora daqui
+        app.launch()
+        
+        let app = XCUIApplication()
+        let nomeTextField = app.textFields["Nome"]
+        nomeTextField.tap()
+        
+        let nomeTextField = XCUIApplication().textFields["Nome"]
+        nomeTextField.tap()
+        nomeTextField.typeText("caue")
+        nomeTextField.tap()
+        app.otherElements.containing(.textField, identifier:"Nome").element.tap()
+        nomeTextField.tap()
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -31,6 +45,10 @@ class TestsProjectUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testLabel() {
+        
     }
     
 }
